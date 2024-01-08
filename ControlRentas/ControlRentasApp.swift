f80159a3c46d13133b7ee748bd23d1bff4620f26
David Zárate <davidzarate33@gmail.com>
2024-01-07 19:10:36 -0600

    Initial Commit

---
+++ ControlRentas/ControlRentasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlRentasApp.swift
+//  ControlRentas
+//
+//  Created by Francisco David Zárate Vásquez on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ControlRentasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
