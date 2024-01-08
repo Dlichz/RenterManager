3c547bb59cb33b0d4c72dca8dfcb87560c5df5bd
David Zárate <davidzarate33@gmail.com>
2024-01-07 20:54:56 -0600

    Se crea lista de inquilinos

---
+++ ControlRentas/Inquilino.swift
@@ -0,0 +1,8 @@
+//
+//  Inquilino.swift
+//  ControlRentas
+//
+//  Created by Francisco David Zárate Vásquez on 07/01/24.
+//
+
+import Foundation
---
+++ ControlRentas/InquilinoCelda.swift
@@ -0,0 +1,18 @@
+//
+//  InquilinoCelda.swift
+//  ControlRentas
+//
+//  Created by Francisco David Zárate Vásquez on 07/01/24.
+//
+
+import SwiftUI
+
+struct InquilinoCelda: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InquilinoCelda()
+}
